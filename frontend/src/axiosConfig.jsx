69a8fd7a363ef1ca95aa53a2c97d43c02ef7a53f
Eleanor Pratt <eleanorpratt@outlook.com>
2025-08-13 15:38:50 +1000

    Fixed Server Address Typo EP-59

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://172.31.97.177:5001', // live
+  baseURL: 'http://3.107.196.202:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
