f34b0a078e38170a7bce84ec659980d41d32ffb7
Eleanor Pratt <eleanorpratt@outlook.com>
2025-08-27 17:22:22 +1000

    updated public IP

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.107.196.202:5001', // live
+  baseURL: 'http://13.211.152.44:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
